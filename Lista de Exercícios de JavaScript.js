3b21fb03bb3fc91cd2b6799a6800591d8b80b1f8
Sofiz <sofia.romano@aluno.ifsp.edu.br>
2025-04-10 14:48:13 -0300

    Create Lista de Exercícios de JavaScript.js

---
+++ Lista de Exercícios de JavaScript.js
@@ -0,0 +1,12 @@
+//1. Completar a função para somar dois números
+function somar(a, b) {
+return a + b;
+}
+console.log(somar(5, 3)); // 8
+//divisor de codigo...............................................................................................
+2. Criar uma função que retorne 'Par' ou 'Ímpar'
+function parOuImpar(numero) {
+return numero % 2 === 0 ? 'Par' : 'Ímpar';
+}
+console.log(parOuImpar(4)); // 'Par'
+//divisor de codigo...............................................................................................
